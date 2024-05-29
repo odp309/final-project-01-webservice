637d31c909a8ff9dd64ad5475f021d9c9a4db9b0
daffaadkha22 <adkhadaffa@gmail.com>
2024-05-29 14:34:58 +0700

    feat changes : change parameter from path to response body
    request body : { accountNumber: '12345' }

---
+++ src/main/java/com/bni/finalproject01webservice/dto/BankAccountRequestDTO.java
@@ -0,0 +1,14 @@
+package com.bni.finalproject01webservice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class BankAccountRequestDTO {
+
+    private String accountNumber;
+}
--- src/main/java/com/bni/finalproject01webservice/interfaces/BankAccountInterface.java
+++ src/main/java/com/bni/finalproject01webservice/interfaces/BankAccountInterface.java
@@ -3,5 +3,5 @@ package com.bni.finalproject01webservice.interfaces;
 import com.bni.finalproject01webservice.dto.BankAccountResponseDTO;
 
 public interface BankAccountInterface {
-    BankAccountResponseDTO getSaldo(String noRekening);
+    BankAccountResponseDTO getSaldo(String accountNumber);
 }
