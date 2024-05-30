41a72cc738984115fdc90c4ffefe3a613a87e2a8
ilhamAdi <apilham1@gmail.com>
2024-05-30 13:39:57 +0700

    feat : add feature getWallet
     This endpoint requires a request body containing the account number. It will return a response listing the wallets associated with that account and their balances.

---
+++ src/main/java/com/bni/finalproject01webservice/dto/WalletRequestDTO.java
@@ -0,0 +1,12 @@
+package com.bni.finalproject01webservice.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WalletRequestDTO {
+
+    private String accountNumber;
+}
