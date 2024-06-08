feb495c5cb7be12d2c4888e872523214c82af7d1
ilhamAdi <apilham1@gmail.com>
2024-06-09 00:33:20 +0700

    add feature init BranchReserve

--- src/main/java/com/bni/finalproject01webservice/interfaces/BranchReserveInterface.java
+++ src/main/java/com/bni/finalproject01webservice/interfaces/BranchReserveInterface.java
@@ -3,9 +3,7 @@ package com.bni.finalproject01webservice.interfaces;
 import com.bni.finalproject01webservice.dto.branch_reserve.request.InitBranchReserveRequestDTO;
 import com.bni.finalproject01webservice.dto.init.response.InitResponseDTO;
 
-public interface BranchInterface {
+public interface BranchReserveInterface {
 
-    InitResponseDTO initBranch();
-
-
+    InitResponseDTO initBranchReserve(InitBranchReserveRequestDTO request);
 }
