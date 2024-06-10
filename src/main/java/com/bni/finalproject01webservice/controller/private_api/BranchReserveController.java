7571c55d9dc1cd929798f51c470fe2e4181541f8
krisnadp <krisna10permana@gmail.com>
2024-06-10 15:12:00 +0700

    feat: add feateure get all branch reserve

--- src/main/java/com/bni/finalproject01webservice/controller/private_api/BranchReserveController.java
+++ src/main/java/com/bni/finalproject01webservice/controller/private_api/BranchReserveController.java
@@ -2,14 +2,18 @@ package com.bni.finalproject01webservice.controller.private_api;
 
 import com.bni.finalproject01webservice.dto.branch_reserve.request.AddBranchReserveRequestDTO;
 import com.bni.finalproject01webservice.dto.branch_reserve.request.AddStockRequestDTO;
+import com.bni.finalproject01webservice.dto.branch_reserve.request.GetBranchReserveRequestDTO;
 import com.bni.finalproject01webservice.dto.branch_reserve.response.AddStockResponseDTO;
 import com.bni.finalproject01webservice.dto.branch_reserve.response.BranchReserveResponseDTO;
+import com.bni.finalproject01webservice.dto.branch_reserve.response.GetBranchReserveResponseDTO;
 import com.bni.finalproject01webservice.interfaces.BranchReserveInterface;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/private/branch-reserve")
@@ -30,4 +34,11 @@ public class BranchReserveController {
     public AddStockResponseDTO addStock(@RequestBody AddStockRequestDTO request) {
         return branchReserveService.addStockBranchReserve(request);
     }
+
+    @PostMapping("/get")
+    @PreAuthorize("hasRole('ADMIN')")
+    public List<GetBranchReserveResponseDTO> getBranchReserve(@RequestBody GetBranchReserveRequestDTO request) {
+        return branchReserveService.getBranchReserveList(request);
+    }
+
 }
--- src/main/java/com/bni/finalproject01webservice/service/BranchReserveService.java
+++ src/main/java/com/bni/finalproject01webservice/service/BranchReserveService.java
@@ -2,9 +2,11 @@ package com.bni.finalproject01webservice.service;
 
 import com.bni.finalproject01webservice.dto.branch_reserve.request.AddBranchReserveRequestDTO;
 import com.bni.finalproject01webservice.dto.branch_reserve.request.AddStockRequestDTO;
+import com.bni.finalproject01webservice.dto.branch_reserve.request.GetBranchReserveRequestDTO;
 import com.bni.finalproject01webservice.dto.branch_reserve.response.AddStockResponseDTO;
 import com.bni.finalproject01webservice.dto.branch_reserve.response.BranchReserveResponseDTO;
-import com.bni.finalproject01webservice.dto.init.response.InitResponseDTO;
+import com.bni.finalproject01webservice.dto.branch_reserve.response.GetBranchReserveResponseDTO;
+import com.bni.finalproject01webservice.dto.employee.response.EmployeeResponseDTO;
 import com.bni.finalproject01webservice.interfaces.BranchReserveInterface;
 import com.bni.finalproject01webservice.model.Branch;
 import com.bni.finalproject01webservice.model.BranchReserve;
@@ -17,6 +19,8 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -65,9 +69,6 @@ public class BranchReserveService implements BranchReserveInterface {
         if (currentBranchReserve == null) {
             throw new RuntimeException("Branch Reserve not found!");
         }
-//
-//        Branch branch = branchRepository.findByName(request.getBranchName());
-//        Currency currency = currencyRepository.findByCode(request.getCurrencyCode());
 
         BigDecimal updatedBalance = currentBranchReserve.getBalance().add(request.getBalance());
         currentBranchReserve.setBalance(updatedBalance);
@@ -75,11 +76,29 @@ public class BranchReserveService implements BranchReserveInterface {
         BigDecimal updatedBalanceTemp = currentBranchReserve.getTempBalance().add(request.getBalance());
         currentBranchReserve.setTempBalance(updatedBalanceTemp);
 
+        currentBranchReserve.setUpdatedAt(new Date());
+
         branchReserveRepository.save(currentBranchReserve);
 
         response.setMessage("Success");
 
         return response;
 
+    }
+
+    @Override
+    public List<GetBranchReserveResponseDTO> getBranchReserveList(GetBranchReserveRequestDTO request) {
+        List<BranchReserve> branchReserveList = branchReserveRepository.findByBranchName(request.getBranchName());
+
+        return branchReserveList.stream()
+                .map(branchReserve -> {
+                    GetBranchReserveResponseDTO response = new GetBranchReserveResponseDTO();
+                    response.setCurrencyCode(branchReserve.getCurrency().getCode());
+                    response.setCurrencyName(branchReserve.getCurrency().getName());
+                    response.setBalance(branchReserve.getBalance());
+                    response.setUpdatedAt(branchReserve.getUpdatedAt());
+                    return response;
+                })
+                .collect(Collectors.toList());
     }
 }
